816dd5b45bc0236e574df8fed2d8e2908e096bf2
Mary Ngigi <wanjikungigi588@gmail.com>
2025-09-04 11:06:29 -0700

    Fix syntax error in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -48,8 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-      ,
-
 ]
 
 ROOT_URLCONF = 'myproject.urls'
